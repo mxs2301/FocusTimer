14f688604794ed13d2cfbae2dfdca6f439f197c3
Max Schopf <schopf.max@tutanota.com>
2024-05-05 18:06:50 +0200

    Created provisional cli-menu
    The menu should just test how one could setup the timer and will afterwards be replaced by a GUI option.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,62 @@
-use std::thread;
-use std::thread::sleep;
-use std::time::Duration;
-fn main() {
-    println!("Hello, world!");
-    let handle = thread::spawn(|| {
-        wait("A");
-    });
-    let second = thread::spawn(|| {
-        wait("B");
-    });
+#![feature(duration_constructors)]
+mod timer;
 
-    second.join().unwrap();
+use std::env;
+use std::io::stdin;
+use std::process::exit;
+use std::{
+    io::{self, Read},
+    time::Duration,
+};
+use timer::timer_mockup;
+
+fn main() {
+    
+    let x: Vec<String>  = env::args().collect();
+
+    if x[1] == "cli".to_string(){
+        if let Ok(x) = cli_mode(){
+            exit(x)
+        }
+
+    }else{
+        exit(1);
+    }
+
+
+
+
+
 }
 
-fn wait<T: std::fmt::Display>(x: T) {
-    let y = Duration::from_secs(2);
+fn cli_mode()-> io::Result<i32>{
     loop {
-        println!("{}", x);
-        sleep(y);
+        println!("Focus Timer - CLI Mode");
+        println!("Press q to quit");
+        println!("Press s to start");
+        let mut buf = String::new();
+        let _ =  stdin().read_line(&mut buf);
+        match buf.as_str().trim() {
+            "q" =>{
+                break;
+            }
+            _ => {
+                buf.clear();
+                continue;
+            }
+        }
+    }
+
+    Ok(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mockup_test() {
+        let durration = Duration::from_mins(1);
+        assert_eq!((0, 0), timer_mockup(durration));
     }
 }
