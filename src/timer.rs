39fcf9cb2b4a7ac8a15f51cb82cfd27a84671d5d
Max Schopf <schopf.max@tutanota.com>
2024-05-06 12:17:10 +0200

    Added break
    Added a new section get custom break time as input and passed it to the
    timer flow

--- src/timer.rs
+++ src/timer.rs
@@ -24,9 +24,41 @@ pub fn timer_mockup(duration: Duration) -> (u64, u64) {
     (minutes, seconds)
 }
 
+pub fn start() {
+    let (duration, amount, break_time) = get_time();
+
+    for _ in 0..amount {
+        timer(duration);
+        println!("Break");
+        timer(break_time as i16);
+    }
+}
+
+fn timer(durr: i16) {
+    let duration = Duration::from_mins(durr as u64);
+
+    let mut countdown = duration.as_secs();
+
+    let mut minutes = duration.as_secs() / 60;
+
+    let mut seconds = 0;
+    while countdown != 0 {
+        println!("{}:{}", minutes, seconds);
+
+        if seconds == 0 {
+            seconds = 59;
+            minutes -= 1;
+        } else {
+            seconds -= 1;
+        }
+        countdown -= 1;
+        sleep(Duration::from_secs(1));
+    }
+}
+
 /// Use to get user input to setup necessary durations
 /// Returns are signed integers as unsigned could panic on a false input
-pub fn get_time() -> (i16, i8) {
+pub fn get_time() -> (i16, i8, i8) {
     let mut buf = String::new();
 
     loop {
@@ -47,9 +79,18 @@ pub fn get_time() -> (i16, i8) {
         // let amount: u8 = buf.trim().parse().expect("Conversion failed");
         let amount: i8 = buf.trim().to_string().parse().expect("Conversion failed");
 
+        buf.clear();
+
+        println!("Please type in the the duration of a break");
+        if let Err(err) = io::stdin().read_line(&mut buf) {
+            eprintln!("{}", err);
+        }
+
+        let break_time: i8 = buf.trim().to_string().parse().expect("Conversion failed");
+
         // Check to see if numbers match
-        if durr > 0 && amount > 0 {
-            return (durr, amount);
+        if durr > 0 && amount > 0 && break_time > 0 {
+            return (durr, amount, break_time);
         } else {
             if let Err(err) = clear() {
                 eprintln!("{}", err);
