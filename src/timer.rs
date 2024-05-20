e879ca1c23366d88233e3c5a19dc5d0225251403
Max Schopf <schopf.max@tutanota.com>
2024-05-21 01:22:38 +0200

    Created(Structure): Wrote setup details
    Created a setup structure to get user input specifying the details on
    the durations for the breaks, timer duration and amount of sessions.

--- src/timer.rs
+++ src/timer.rs
@@ -1,6 +1,8 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, Write};
 use std::{thread::sleep, time::Duration};
-
-pub fn timer_mockup(duration: Duration) -> (u64, u64) {
+fn timer(duration: Duration) -> (u64, u64) {
     let mut countdown = duration.as_secs();
 
     let mut minutes = duration.as_secs() / 60;
@@ -20,16 +22,36 @@ pub fn timer_mockup(duration: Duration) -> (u64, u64) {
     }
 
     (minutes, seconds)
-}pub fn start(){
+}
+pub fn start() {}
 
-    use crate::audio::play;
-    println!("This is fun");
+fn setup_session_details() -> Result<(i32, i32, i32), Box<dyn Error>> {
+    let mut buf = String::new();
 
+    print!("How long should a session be: ");
+    io::stdout().flush()?;
+    let _ = io::stdin().read_line(&mut buf);
+    let session = buf.trim().parse::<i32>()?;
+    buf.clear();
 
+    print!("How long should a break be: ");
+    io::stdout().flush()?;
+    let _ = io::stdin().read_line(&mut buf);
+    let pause = buf.trim().parse::<i32>()?;
+    buf.clear();
+
+    print!("How many rounds do you want to have: ");
+    io::stdout().flush()?;
+    let _ = io::stdin().read_line(&mut buf);
+    let amount = buf.trim().parse::<i32>()?;
+
+    Ok((session, pause, amount))
+}
+
+fn setup_audio() -> Result<File, Box<dyn Error>> {
     let path = std::path::Path::new("/home/max/Projects/FocusTimer/media/success.mp3");
-    let file = std::fs::File::open(path).unwrap();
 
-    let _ = play(file);
+    let file = File::open(path)?;
 
-
+    Ok(file)
 }
