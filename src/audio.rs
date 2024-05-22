1f78854c5eb7e743a3e421db18597e9f6d6830ad
Max Schopf <schopf.max@tutanota.com>
2024-05-22 22:31:31 +0200

    Reformatted file

--- src/audio.rs
+++ src/audio.rs
@@ -1,22 +1,16 @@
 use rodio::*;
-use std::collections::HashMap;
-use std::io::BufReader;
-use std::fs::File;
 use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 
+pub fn play(file: File) -> Result<(), Box<dyn Error>> {
+    let (_stream, handle) = rodio::OutputStream::try_default()?;
+    let sink = rodio::Sink::try_new(&handle)?;
+    sink.append(Decoder::new(BufReader::new(file))?);
+    clearscreen::clear()?;
+    println!("Sound of break");
 
-pub fn play(file : File)-> Result<(), Box<dyn Error>> {
-   
-   let (_stream, handle) = rodio::OutputStream::try_default()?;
+    sink.sleep_until_end();
 
-   let sink = rodio::Sink::try_new(&handle)?;
-
-   //let file = std::fs::File::open("/home/max/Projects/FocusTimer/media/success.mp3")?;
-
-   sink.append(Decoder::new(BufReader::new(file))?);
-   clearscreen::clear()?;
-   println!("Sound of break");
-   sink.sleep_until_end();
-
-   Ok(())
+    Ok(())
 }
